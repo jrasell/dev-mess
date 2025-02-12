b24bb7aaff20087ffbc847465ac1444c9f7a4769
James Rasell <jamesrasell@gmail.com>
2025-02-12 12:40:34 +0000

    multipass: Add Nomad server cluster configuration.

---
+++ nomad/lab/multipass/nomad-server-cluster/main.tf
@@ -0,0 +1,33 @@
+module "nomad_server" {
+  source             = "../../shared/terraform/multipass-compute"
+
+  ansible_group_name   = "nomad_server"
+  instance_count       = 3
+  instance_memory      = "4GiB"
+  instance_name_prefix = "nomad-server"
+  instance_ssh_key     = file("~/.ssh/id_rsa.pub")
+}
+
+module "ansible_provision" {
+  source     = "../../shared/terraform/ansible-provision"
+  depends_on = [module.nomad_server]
+
+  ansible_inventory_path = abspath("./inventory.yaml")
+  ansible_playbook_path  = abspath("./playbook_nomad_server.yaml")
+}
+
+output "details" {
+  value = <<EOH
+SSH commands:
+  Nomad Servers:
+%{for ip in module.nomad_server.instance_ips~}
+    - ssh ${ip}
+%{endfor~}
+
+Rsync commands:
+  Nomad Servers:
+%{for ip in module.nomad_server.instance_ips~}
+    - rsync -r --exclude 'nomad/ui/node_modules/*' /Users/jrasell/Projects/Go/nomad jrasell@${ip}:/home/jrasell/
+%{endfor~}
+EOH
+}
