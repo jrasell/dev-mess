b62b1fd4221e1cd6d24d6cb483d73f9a81978e82
James Rasell <jamesrasell@gmail.com>
2025-05-13 08:32:33 +0100

    workstation: add initial asdf plugin installs

--- workstation/setup.sh
+++ workstation/setup.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+function install_asdf_plugins() {
+
+  # Install and globally set Golang.
+  asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+  asdf install golang 1.24.2
+  asdf set -u golang 1.24.2
+
+  # Install and globally set Terraform.
+  asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
+  asdf install terraform 1.11.4
+  asdf set -u terraform 1.11.4
+
+  # Install and globally set Consul.
+  asdf plugin add consul https://github.com/asdf-community/asdf-hashicorp.git
+  asdf install consul 1.20.5
+  asdf set -u consul 1.20.5
+
+  # Install and globally set NodeJS and Yarn.
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf install nodejs 20.19.1
+  asdf set -u nodejs 20.19.1
+
+  asdf install yarn 1.22.22
+  asdf set -u yarn 1.22.22
+}
+
 function install_helix_go_packages() {
   go install golang.org/x/tools/cmd/goimports@latest
   go install golang.org/x/tools/gopls@latest
@@ -16,6 +42,7 @@ function install_helix_config_files() {
   cp .config/helix/languages.toml "$HOME"/.config/helix/languages.toml
 }
 
+install_asdf_plugins
 install_helix_go_packages
 install_helix_rust_packages
 install_helix_config_files
